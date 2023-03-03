8b4bc6617d35b1bc9b62fbdd2dc5ae9049f16b00
Aliona83 <alionajanusko@gmail.com>
2023-03-03 12:32:31 +0000

    Add function to get random number

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct number 😀'
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct number 😀'
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 25;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 25;
+// console.log(document.querySelector('.guess').value);
+
+const number = Math.trunc(Math.random() *20)+1;
+console.log(number)
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = document.querySelector('.guess').value;
+    console.log(guess)
+
+    if(!guess) {
+        document.querySelector('.message').textContent = ' No number enter ⛔️';
+    }
+});
