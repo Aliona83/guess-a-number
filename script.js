917fb4a2d3ad9a903384fea8fcbd4b0f4810678f
Aliona83 <alionajanusko@gmail.com>
2023-03-03 13:00:57 +0000

    Add if statement to check right guess

--- script.js
+++ script.js
@@ -8,14 +8,25 @@
 // document.querySelector('.guess').value = 25;
 // console.log(document.querySelector('.guess').value);
 
-const number = Math.trunc(Math.random() *20)+1;
-console.log(number)
+const secretNumber = Math.trunc(Math.random() *20)+1;
+let = 20;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function(){
-    const guess = document.querySelector('.guess').value;
+    const guess = Number(document.querySelector('.guess').value);
     console.log(guess)
 
     if(!guess) {
         document.querySelector('.message').textContent = ' No number enter ⛔️';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number 🎉'; 
+    } else if(guess > secretNumber) {
+  document.querySelector('.message').textContent = 'Too hight !'
+  score--;
+  document.querySelector('.score').textContent = score;
+    } else if(guess < secretNumber) {
+        document.querySelector('.message').textContent = 'Too low !'
+        score--;
+        document.querySelector('.score').textContent = score;
     }
 });
